8a29f09f9f5b7f7357dd198802f6f56a72ad3600
Nicolas Seignette <nicolasseignette@gmail.com>
2021-03-06 17:03:39 +0100

    Add Auth

---
+++ src/Controller/ConversationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Participant;
+use App\Repository\ConversationRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+     * @Route("/conversations", name="conversations.")
+     */
+class ConversationController extends AbstractController
+{
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var ConversationRepository
+     */
+    private $conversationRepository;
+
+    public function __construct(UserRepository $userRepository,
+                                EntityManagerInterface $entityManager,
+                                ConversationRepository $conversationRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->conversationRepository = $conversationRepository;
+    }
+    
+    /**
+     * @Route("/", name="newConversations", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function index(Request $request)
+    {
+        $otherUser = $request->get('otherUser', 0);
+        $otherUser = $this->userRepository->find($otherUser);
+        return $this->json();
+    }
+}
